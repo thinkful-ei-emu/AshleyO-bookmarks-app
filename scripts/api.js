8f1d763ed777b3bbf0553e1e938eeceb00980478
aoelbaum <ashleyoelbaum@gmail.com>
2019-06-15 19:36:21 -0400

    working on api functionality, need to get rid of expand property on book mark

---
+++ scripts/api.js
@@ -0,0 +1,90 @@
+'use strict';
+// eslint-disable-next-line no-unused-vars
+
+const api = (function(){
+    const Base_URL = 'https://thinkful-list-api.herokuapp.com/ashley';
+
+
+    const listApiFetch = function(...args) {
+        let error;
+        return fetch(...args)
+        .then(res => {
+            if (!res.ok) {
+                error = {code: res.status};
+                if(!res.headers.get('content-type').includes('json')) {
+                    error.message = res.statusText;
+                    return Promise.reject(error);
+                }
+                
+            }
+            return Promise.reject(error);
+        })
+        .then(data => {
+            if (error) {
+                error.message = data.message;
+                return Promise.reject(error);
+            }
+            return data;
+        });
+        
+    };
+
+    const getBookmarks = function () {
+        return listApiFetch(BASE_URL + '/bookmarks')
+    };
+
+    const createBookmark = function(newBookmark) {
+        const jsonNewBookmark = JSON.stringify(newBookmark);
+        return listApiFetch(Base_URL + '/bookmarks', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonNewBookmark
+        });                    
+    };
+    
+    const updateBookmark = function(id, updateData) {
+        const newData = JSON.stringify(updateData);
+        return listApiFetch(Base_URL + '/bookmarks/' + id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: newData
+        });                    
+    };
+
+
+    const deleteBookmark = function(id) {        
+        return listApiFetch(Base_URL + '/bookmarks/' + id, {
+            method: 'DELETE',
+            
+        });                    
+    };
+    
+    
+
+
+    
+
+
+
+
+
+
+
+    return {
+        getBookmarks,
+        createBookmark,
+        updateBookmark,
+        deleteBookmark,
+
+    };
+
+
+
+
+
+
+}());
--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -4,190 +4,237 @@
 
 const bookmarkList = (function(){
 
+    function generateError(message) {
+        return `
+        <section class="error-content">
+        <button id="cancel-error">X/button>
+        <p>${message}</p>
+        </section>`;
+    } 
+        
+    function renderError() {
+        if(store.error) {
+            const errorDisplay = generateError(store.error);
+            $('.error-container').html(errorDisplay);
+        }
+            else {
+                $('.error-container').empty();
+            }
+        }
 
-function generateStars(bookmark){
-    console.log(bookmark.rating);
-    let stars = '';
-    for (let i=1; i <= bookmark.rating; i++){
-        stars += `<i class="fas fa-star"></i>`        
+    
+
+
+    function generateStars(bookmark){
+        console.log(bookmark.rating);
+        let stars = '';
+        for (let i=1; i <= bookmark.rating; i++){
+            stars += `<i class="fas fa-star"></i>`        
+        }
+        return stars;
     }
-    return stars;
-}
 
-function generateBookmarkElement(bookmark){
-    console.log('generateBookmarkElement ran');    
-     
-    const bookmarkRating = generateStars(bookmark);
-    
-    return `
-    <li class="bookmark-element" data-bookmark-id="${bookmark.id}">    
-    <span class="bookmark-element-title">${bookmark.title}</span>    
-    
-    <div class="star-rating">   
-        <span>${bookmarkRating}</span>
-    </div>
-    <div class="show-url hidden">
-       <span> ${bookmark.url}</span><br>
-       <span> ${bookmark.description}</span>       
-    </div>
-    
-    <div class= "bookmark-buttons">
-        <button class="js-bookmark-expand"> 
-            <span class="button-label">Expand</span>
-        </button>
-        <button class="js-bookmark-delete"> 
-            <span class="button-label">Remove</span>
-        </button>
-    </div>
-    </li>`;
-}
+    function generateBookmarkElement(bookmark){
+        console.log('generateBookmarkElement ran');    
+        
+        const bookmarkRating = generateStars(bookmark);
+        
+        return `
+        <li class="bookmark-element" data-bookmark-id="${bookmark.id}">    
+        <span class="bookmark-element-title">${bookmark.title}</span>    
+        
+        <div class="star-rating">   
+            <span>${bookmarkRating}</span>
+        </div>
+        <div class="show-url hidden">
+        <span> ${bookmark.url}</span><br>
+        <span> ${bookmark.description}</span>       
+        </div>
+        
+        <div class= "bookmark-buttons">
+            <button class="js-bookmark-expand"> 
+                <span class="button-label">Expand</span>
+            </button>
+            <button class="js-bookmark-delete"> 
+                <span class="button-label">Remove</span>
+            </button>
+        </div>
+        </li>`;
+    }
 
-function generateBookmarksString(bookmarkList){
-    console.log('generateBookmarkstring ran')
-    const bookmarks = bookmarkList.map((bookmark) => generateBookmarkElement(bookmark));    
-    return bookmarks.join('');
-}
+    function generateBookmarksString(bookmarkList){
+        console.log('generateBookmarkstring ran')
+        const bookmarks = bookmarkList.map((bookmark) => generateBookmarkElement(bookmark));    
+        return bookmarks.join('');
+    }
 
-function render() {
-    let bookmarks = [...store.bookmarks]    
-    console.log('render ran');      
-     if(store.filterRating != null){        
-         bookmarks = bookmarks.filter(bookmark => bookmark.rating >= store.filterRating);
-         console.log(`NEW BOOKMARKS ARRAY: ${bookmarks}`); 
 
-     };
+    function render() {
+        renderError();
+        let bookmarks = [...store.bookmarks]    
+        console.log('render ran');      
+        if(store.filterRating != null){        
+            bookmarks = bookmarks.filter(bookmark => bookmark.rating >= store.filterRating);
+            console.log(`NEW BOOKMARKS ARRAY: ${bookmarks}`); 
 
-    const bookmarkListString = generateBookmarksString(bookmarks); 
-    $('.js-bookmarks-list').html(bookmarkListString);
-    
-}
+        };
 
-function serializeJson(form) {    
-    const formData = new FormData(form);
-    const object = {};
-    formData.forEach((val, name) => object[name] = val);
-    console.log(formData);
-    return JSON.stringify(object);
-    //need to send to post request
-}
+        const bookmarkListString = generateBookmarksString(bookmarks); 
+        $('.js-bookmarks-list').html(bookmarkListString);
+        
+    }
 
-function handleNewBookmarkSubmit() {      
-    $('#js-bookmarks-form').submit(event => {          
-        event.preventDefault();
-        console.log('handleNewBookmarkSubmit ran');          
-        let formElement = $('#js-bookmarks-form')[0];      
-        let jsonElement = serializeJson(formElement);
-        // console.log(jsonElement);
-        const bookmark = {
-            id: cuid(), 
-            title: $(`#title`).val(),           
-            rating: $(`#ratingScale`).val(),
-            url: $(`#url`).val(),
-            description: $(`#description`).val(),
-            expand: false,
-         };                
-        formElement.reset();
-          
-        if(event.currentTarget && store.adding){                                     
-            store.toggleAddBookmark();            
-            store.addBookmark(bookmark);                                
-            $('.create-bookmark').addClass('hidden');
-            $('.minRating-container').removeClass('hidden');  
+    // function serializeJson(form) {    
+    //     const formData = new FormData(form);
+    //     const object = {};
+    //     formData.forEach((val, name) => object[name] = val);
+    //     console.log(formData);
+    //     return JSON.stringify(object);
+    //     //need to send to post request
+    // }
+
+    function handleNewBookmarkSubmit() {      
+        $('#js-bookmarks-form').submit(event => {          
+            event.preventDefault();
+            console.log('handleNewBookmarkSubmit ran');          
+            let bookmarkForm = $('#js-bookmarks-form')[0];      
+            // let newBookmark = serializeJson(bookmark);
             
-            render();
-        }
-        else {     
-            store.toggleAddBookmark();
-            $('.create-bookmark').removeClass('hidden');
-            $('.minRating-container').addClass('hidden');
-               
-        }
-        
-        
-    });
-}
+            const newBookmark = {
+                id: cuid(), 
+                title: $(`#title`).val(),           
+                rating: $(`#ratingScale`).val(),
+                url: $(`#url`).val(),
+                description: $(`#description`).val(),
+                expand: false,                
+            };
+            console.log(newBookmark);  
+            bookmarkForm.reset();
+            if(event.currentTarget && store.adding){
+                api.createBookmark(newBookmark)
+                .then((bookmark) => {                                                        
+                        store.toggleAddBookmark();            
+                        store.addBookmark(bookmark);                                
+                        $('.create-bookmark').addClass('hidden');
+                        $('.minRating-container').removeClass('hidden');                        
+                        render();
+                    
+                })
+                .catch((err) => {
+                    store.setError(err.message);
+                    renderError();
+                  })
+                ;
+
+            }
+            else {     
+                store.toggleAddBookmark();
+                $('.create-bookmark').removeClass('hidden');
+                $('.minRating-container').addClass('hidden');   
+            }
+
+            
+
+            
 
 
-
-//get id of current bookmark
-function getCurrentBookmarkId(currentBookmark) {    
-    return $(currentBookmark)
-    .closest('.bookmark-element')
-    .data('bookmark-id'); 
-    
-        
-
-}
-//event listener placed on remove button to remove bookmark when clicked
-function handleDeleteBookmark() {
-    $('.js-bookmarks-list').on('click','.js-bookmark-delete', event =>{        
-        const id = getCurrentBookmarkId(event.currentTarget);
-        store.findAndDelete(id);
-        render();
-    });
-}
-
-const toggleExpandStatus = function(bookmark) {
-    
-    if(bookmark.expand){
-        bookmark.expand = false;
-        console.log(`toggleStatus: IF: ${bookmark.expand}`);
-    }
-    else {
-        bookmark.expand = true;
-        console.log(`toggleStatus ELSE: ${bookmark.expand}`)
-    }
-
-    return bookmark.expand;
-    
-}
-//user can click on addbookmark button to input bookmark info and add bookmark to list
-function handleExpandButton (){ 
-    $('.js-bookmarks-list').on('click','.js-bookmark-expand', event =>{        
-                   
-        const id = getCurrentBookmarkId(event.currentTarget);
-        const bookmark = store.findById(id);  
-        console.log(bookmark);
-        const expandStatus = toggleExpandStatus(bookmark);        
+            
            
-        if(expandStatus) {
-            $('.show-url').removeClass('hidden');            
-            console.log("removeclass",expandStatus);                
+            
+           
+            
+        });
+    }
+
+
+
+    //get id of current bookmark
+    function getCurrentBookmarkId(currentBookmark) {    
+        return $(currentBookmark)
+        .closest('.bookmark-element')
+        .data('bookmark-id'); 
+        
+            
+
+    }
+    //event listener placed on remove button to remove bookmark when clicked
+    function handleDeleteBookmark() {
+        $('.js-bookmarks-list').on('click','.js-bookmark-delete', event =>{        
+            const id = getCurrentBookmarkId(event.currentTarget);
+            store.findAndDelete(id);
+            render();
+        });
+    }
+
+    const toggleExpandStatus = function(bookmark) {
+        
+        if(bookmark.expand){
+            bookmark.expand = false;
+            console.log(`toggleStatus: IF: ${bookmark.expand}`);
         }
         else {
-            $('.show-url').addClass('hidden');
-            console.log("addlclass",expandStatus);                
+            bookmark.expand = true;
+            console.log(`toggleStatus ELSE: ${bookmark.expand}`)
         }
-       
+
+        return bookmark.expand;
         
-    });
-    
-}
-//selected mim rating
-function handleMinRatingFilter() {
-    $('.container').on('change','#ratingMin', event =>{       
-           event.preventDefault();
-           let filterRatingVal = $(event.currentTarget).val();
-           console.log(filterRatingVal);    
-           store.setfilterRating(filterRatingVal);                  
-           render();       
-    });
-}
+    }
+    //user can click on addbookmark button to input bookmark info and add bookmark to list
+    function handleExpandButton (){ 
+        $('.js-bookmarks-list').on('click','.js-bookmark-expand', event =>{        
+                    
+            const id = getCurrentBookmarkId(event.currentTarget);
+            const bookmark = store.findById(id);  
+            console.log(bookmark);
+            const expandStatus = toggleExpandStatus(bookmark);        
+            
+            if(expandStatus) {
+                $('.show-url').removeClass('hidden');            
+                console.log("removeclass",expandStatus);                
+            }
+            else {
+                $('.show-url').addClass('hidden');
+                console.log("addlclass",expandStatus);                
+            }
+        
+            
+        });
+        
+    }
+    //selected mim rating
+    function handleMinRatingFilter() {
+        $('.container').on('change','#ratingMin', event =>{       
+            event.preventDefault();
+            let filterRatingVal = $(event.currentTarget).val();
+            console.log(filterRatingVal);    
+            store.setfilterRating(filterRatingVal);                  
+            render();       
+        });
+    }
+
+    function handleCloseError () {
+        $('.error-container').on('click', `#cancel-error`, () => {
+            store.setError(null);
+            renderError();
+        })
+    }
 
 
 
 
-function bindEventListeners() {    
-    handleNewBookmarkSubmit();    
-    handleExpandButton();    
-    handleDeleteBookmark();
-    handleMinRatingFilter();
-}
+    function bindEventListeners() {    
+        handleNewBookmarkSubmit();    
+        handleExpandButton();    
+        handleDeleteBookmark();
+        handleMinRatingFilter();
+        handleCloseError();
+    }
 
 
-return {
-    render: render,
-    bindEventListeners: bindEventListeners,
-};
+    return {
+        render: render,
+        bindEventListeners: bindEventListeners,
+    };
 
 }());
